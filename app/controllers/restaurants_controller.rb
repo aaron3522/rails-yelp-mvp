3207f5503c633b3849865939e7a94b0e435a9c77
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <aaron3522@hotmail.com>
2023-08-03 20:52:47 +0100

    complete

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,13 @@
+class RestaurantsController < ApplicationController
+    def index
+        @restaurants = Restaurant.all
+    end
+    
+    def show
+    end
+
+    def new
+    end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+get '/restaurants', to: 'restaurants#index'
+get '/restaurants', to: 'restuarants#show'
+get '/restaurants', to: 'restaurants#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
